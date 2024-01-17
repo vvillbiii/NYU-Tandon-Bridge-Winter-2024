7760f62141408afd9e1734ed9559bd992eae7c9d
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-01-17 00:21:05 -0500

    cout and cin for john and bill q3

--- week two /wb886_hw2_q3.cpp
+++ week two /wb886_hw2_q3.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 using namespace std;
 
+const int DAYS = 24;
+const int HOURS = 60;
+
+
 int main()
 {
     int jd,jh, jm, bd, bh, bm, totalDays, totalHours, totalMinutes;
+    
+    // john
     cout<< "Please enter the number of days John has worked:";
     cin >> jd;
+    cout<< "Please enter the number of hours John has worked:";
+    cin >> jh;
+    cout<< "Please enter the number of minutes John has worked:";
+    cin >> jm;
+    
+    //Bill
+      cout<< "Please enter the number of days Bill has worked:";
+    cin >> bd;
+    cout<< "Please enter the number of hours Bill has worked:";
+    cin >> bh;
+    cout<< "Please enter the number of minutes Bill has worked:";
+    cin >> bm;
+
+
     return 0;
 }
