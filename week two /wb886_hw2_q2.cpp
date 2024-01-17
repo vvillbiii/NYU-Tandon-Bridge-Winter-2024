8a5e3631766bc4bcb38a3210e4ea80d6604f1036
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-01-16 23:18:14 -0500

    add dollars and cents to q2

--- week two /wb886_hw2_q2.cpp
+++ week two /wb886_hw2_q2.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 using namespace std;
 
+const int DOLLAR = 100;
 const int QUARTERS = 25; 
 const int DIMES = 10;
 const int NICKELS = 5;
 
 int main()
 {
-
+    int dollar, cents;
+    cout << "Please enter your amount in the format of dollars and cents separated by a space:"<<endl;
+    cin >> dollar>>cents;
+    cout << dollar << " dollars and "<< cents << " cents are:"<<endl;
 
     return 0;   
 }
