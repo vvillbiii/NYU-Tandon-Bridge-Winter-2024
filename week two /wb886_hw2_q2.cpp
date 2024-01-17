41561a315e7f5c398d7a5f693fd65655457d44cf
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-01-17 00:16:34 -0500

    solve q2

--- week two /wb886_hw2_q2.cpp
+++ week two /wb886_hw2_q2.cpp
@@ -8,10 +8,30 @@ const int NICKELS = 5;
 
 int main()
 {
-    int dollar, cents;
+    int dollar, cents, totalCents, quarters, dimes, nickels, pennies;
+    
+    
     cout << "Please enter your amount in the format of dollars and cents separated by a space:"<<endl;
     cin >> dollar>>cents;
     cout << dollar << " dollars and "<< cents << " cents are:"<<endl;
+
+    totalCents = dollar * DOLLAR + cents;
+    
+    quarters = totalCents / QUARTERS;
+    totalCents = totalCents % QUARTERS;
+
+    
+    dimes = totalCents / DIMES;
+    totalCents = totalCents % DIMES;
+
+    
+    nickels = totalCents / NICKELS;
+    totalCents = totalCents % NICKELS;
+
+    
+    pennies = totalCents;
+
+    cout<<quarters<<" quarters, " << dimes << " dimes, " << nickels << " nickels and " <<pennies << " pennies" ;
 
     return 0;   
 }
