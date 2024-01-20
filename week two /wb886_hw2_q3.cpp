0249ef8612f59f8ecb88c45410387ef86050d7ab
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-01-19 21:23:25 -0500

    solve q3

--- week two /wb886_hw2_q3.cpp
+++ week two /wb886_hw2_q3.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 const int DAYS = 24;
-const int HOURS = 60;
 
 
 int main()
@@ -10,21 +9,26 @@ int main()
     int jd,jh, jm, bd, bh, bm, totalDays, totalHours, totalMinutes;
     
     // john
-    cout<< "Please enter the number of days John has worked:";
+    cout<< "Please enter the number of days John has worked: ";
     cin >> jd;
-    cout<< "Please enter the number of hours John has worked:";
+    cout<< "Please enter the number of hours John has worked: ";
     cin >> jh;
-    cout<< "Please enter the number of minutes John has worked:";
+    cout<< "Please enter the number of minutes John has worked: ";
     cin >> jm;
     
     //Bill
-      cout<< "Please enter the number of days Bill has worked:";
+    cout<< "Please enter the number of days Bill has worked: ";
     cin >> bd;
-    cout<< "Please enter the number of hours Bill has worked:";
+    cout<< "Please enter the number of hours Bill has worked: ";
     cin >> bh;
-    cout<< "Please enter the number of minutes Bill has worked:";
+    cout<< "Please enter the number of minutes Bill has worked: ";
     cin >> bm;
 
+    totalMinutes = jm + bm;
+    totalHours = (jh + bh) % DAYS;
+    totalDays = jd + bd + ((jh + bh) / DAYS); 
+
+    cout << "The total time both of them worked together is " << totalDays << " days, "<< totalHours << " hours and "<< totalMinutes << " minutes";
 
     return 0;
 }
