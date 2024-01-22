2d15c7eb79ddd3c5fe7a1d0bb9304e8f10f33841
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-01-22 10:44:55 -0500

    start hw3 prob 4

--- week_three/hw/wb886_hw3_q4.cpp
+++ week_three/hw/wb886_hw3_q4.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 using namespace std;
 
+const int FLOOR_ROUND = 1; 
+const int CEILING_ROUND = 2; 
+const int ROUND = 3;
+
 int main()
 {   
- 
+    double realNum;
+    int method;
+
+    cout << "Please eneter a Real number: " <<endl;
+    cin >> realNum;
+
+    cout << "Choose your rounding method:\n";
+    cout <<"1. Floor round\n";
+    cout <<"2. Ceiling round\n";
+    cout <<"3. Round to the nearest whole number"<<endl;
+    cin >> method;
+
+
+  
+   switch (method) {
+    case FLOOR_ROUND:
+        cout << "Floor round";
+        break;
+    case CEILING_ROUND:
+        cout << "Ceiling round";
+        break;
+    case ROUND:
+        cout << "Round to the nearest whole number";
+        break;
+}
     return 0;
 }
