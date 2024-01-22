2684035cfec422d2fc84a01087f6e3cddc8eba7b
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-01-22 09:59:21 -0500

    solve hw 3 problem 2

--- week_three/hw/wb886_hw3_q1.cpp
+++ week_three/hw/wb886_hw3_q1.cpp
@@ -5,25 +5,32 @@ const double DISCOUNT = 0.10;
 
 int main()
 {   
-    int firstItem, SecondItem, basePrice;
-    double taxRate, discountPrice, totalPrice;
+    int firstItem, SecondItem;
+    double taxRate, discountPrice, totalPrice, basePrice;
     char clubCard;
 
+    cout << "Enter the price of first item: ";
+    cin >> firstItem;
+    cout << "Enter the price of second item: ";
+    cin >> SecondItem;
+    cout << "Does customer have a club card? (Y/N): ";
+    cin >> clubCard;
+    cout << "Enter tax rate, e.g. 5.5 for 5.5% tax: ";
+    cin >> taxRate;
+   
     basePrice = firstItem + SecondItem;
 
-    
-    cout << "Enter the price of first item: "<<endl;
-    cin >> firstItem;
-    cout << "Enter the price of second item: "<<endl;
-    cin >> SecondItem;
-    cout << "Does customer have a club card? (Y/N): "<<endl;
-    cin >> clubCard;
-    cout << "Enter tax rate, e.g. 5.5 for 5.5% tax: "<<endl;
-    cin >> taxRate;
-    cout << "Base price: "<< basePrice <<endl;
-    cout << "Price after discounts: "<<endl;
-    cout << "Total price: "<<endl;
+    if(firstItem > SecondItem && clubCard){
+        firstItem = firstItem / 2;
+    } else {
+        SecondItem = SecondItem / 2;
+    }
 
+ 
     
+
+    cout << "Base price: "<< basePrice <<endl;
+    cout << "Price after discounts: " <<endl;
+    cout << "Total price: "<<endl;
     return 0;
 }
--- week_three/hw/wb886_hw3_q2.cpp
+++ week_three/hw/wb886_hw3_q2.cpp
@@ -3,12 +3,33 @@ using namespace std;
 
 int main()
 {   
+   string name;
+   int gradYear, currYear, diff;
   
-   cout << "Please enter your name: "<<endl;
+   cout << "Please enter your name: ";
+   cin >> name;
     
-    cout << "Please enter your graduation year: "<<endl;
-    
-    cout << "Please enter your current year: "<<endl;
-    
+    cout << "Please enter your graduation year: ";
+    cin >> gradYear;
+
+    cout << "Please enter your current year: ";
+    cin >> currYear;
+
+    diff = gradYear - currYear;
+
+    if (diff > 4){
+        cout << name << ", you are not in college";
+    } else if(diff == 4){
+        cout << name << ", you are a Freshman";
+    } else if( diff == 3){
+        cout <<  name << ", you are a Sophmore";
+    } else if(diff == 2 ){
+        cout <<  name <<  ", you are a Junior";
+    } else if (diff == 1){
+        cout <<  name <<  ", you are a Senior";
+    } else {
+        cout << name << ", you graduated.";
+    }
+
     return 0;
 }
