8cf9a61195eca7f43117de51ce4b8d30bafd3b50
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-01-24 13:01:04 -0500

    start hw3 q6

--- week_three/hw/wb886_hw3_q6.cpp
+++ week_three/hw/wb886_hw3_q6.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include<string>
 using namespace std;
+
+const double PEAK = 0.40;
+const double AFTERHOURS = 0.25;
+const double WEEKEND = 0.15;
 
 int main()
 {   
   
+    int hour, minute, duration;
+    string day;
+    char temp;
+
+    cout << "What day did you make the call? enter first two characters of day.  example (Mo Tu We Th Fr Sa Su): ";
+    cin >> day;
+    cout << "What time did the call start? enter in 24-notation ex. (13:30): ";
+    cin>> hour>>temp>>minute;
+    cout<<" How long did the call last in minutes? ";
+    cin >> duration;
+
+    // check hour input
+
+    //check day
   
     return 0;
 }
