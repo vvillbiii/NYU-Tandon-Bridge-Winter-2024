0ecf8682fcbfeea4f222b071a33b42c2e7fe30b6
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-01-24 11:12:44 -0500

    hw3 scaffold

--- week_three/hw/wb886_hw3_q5.cpp
+++ week_three/hw/wb886_hw3_q5.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 using namespace std;
+#include<string>
+
+const double KILOGRAMS = 0.453592;
+const double METERS = 0.0254;
+
 
 int main()
 {   
+   int weight, height;
+   double bmi, kg, m;
+   string status;
+
+
+   cout << "Please enter weight (in pounds):" ;
+   cin >> weight;
+   cout << "Please enter height (in inches):" ;
+   cin >> height;
+
+   kg = weight * KILOGRAMS;
+   m = height * METERS;
    
+   if(bmi > 18.5)
+        status = "Underweight";
+    else if(bmi == 18.5 || bmi < 25) 
+        status = "Normal";
+    else if(bmi == 25 || bmi < 30) 
+        status = "Overweight";
+    else if(bmi >= 30 )
+        status = "Obese";
+
+
+    cout << "The weight status is: "<< status;
+
     return 0;
 }
