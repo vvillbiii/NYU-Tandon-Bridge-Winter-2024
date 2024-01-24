c468420cde509a2e7c8590a7b494dd51c0c54297
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-01-24 13:42:31 -0500

    start q3 in hw3

--- week_three/hw/wb886_hw3_q3.cpp
+++ week_three/hw/wb886_hw3_q3.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <cmath> 
 using namespace std;
 
 int main()
 {   
+    int a, b, c;
+
+    cout << "Please enter value of a: ";
+    cin >> a;
+    cout << "Please enter value of b: ";
+    cin >> b;
+    cout << "Please enter value of c: ";
+    cin >> c;
+
+
    
     return 0;
 }
