60ed38213e1ceb6660a5e42bba67b7af42fe1208
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-01-29 10:34:10 -0500

    start hw4 q2 and q6

--- week_four/hw/wb886_hw4_q2.cpp
+++ week_four/hw/wb886_hw4_q2.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+// Romandigit I V X L C D M
+// Decimalvalue 1 5 10 50 100 500 1000
 
 int main()
 {
+    int input;
+    string romanDigit;
+    cout <<"Enter decimal number:"<<endl;
+    cin >> input;
 
 
+    while(input != 0){
+
+    }
     return 0;
 }
--- week_four/hw/wb886_hw4_q6.cpp
+++ week_four/hw/wb886_hw4_q6.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main()
 {
+    int input;
+    cout <<"Please enter a positive integer: ";
+    cin >> input;
 
 
     return 0;
