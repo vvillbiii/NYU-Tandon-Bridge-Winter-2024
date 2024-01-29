d0a787a1e6efffdc1d518a1499836d4d26b00031
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-01-29 11:17:31 -0500

    start section a and b for hw4 q4

--- week_four/hw/wb886_hw4_q4.cpp
+++ week_four/hw/wb886_hw4_q4.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main()
 {
+    int sequence;
+    cout << "Section A"<<endl;
+    cout << "Please enter the length of the sequence: ";
+    cin >> sequence;
 
 
+    cout << "Section B"<<endl;
+    cout << "Please enter a non-empty sequence of positive integers,";
+    cout << "each one in a separate line. End your sequence by typing -1:";
     return 0;
 }
