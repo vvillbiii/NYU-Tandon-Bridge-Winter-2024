f3572c19cea494be4d17b34a5e6b149dd5a2ceb2
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-01-30 13:49:28 -0500

    solve hw4 q4 part b

--- week_four/hw/wb886_hw4_q4.cpp
+++ week_four/hw/wb886_hw4_q4.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     int sequence, num, currNum = 1;
+    bool seenNum;
     cout << "Section A"<<endl;
     cout << "Please enter the length of the sequence: ";
     cin >> sequence;
@@ -21,6 +22,22 @@ int main()
 
     cout << "Section B"<<endl;
     cout << "Please enter a non-empty sequence of positive integers,";
-    cout << "each one in a separate line. End your sequence by typing -1:";
+    cout << "each one in a separate line. End your sequence by typing -1:" <<endl;
+
+    seenNum = false;
+    currNum = 1;
+    sequence = 0;
+    while(seenNum == false){
+        cin>>num;
+        if(num == -1){
+            seenNum = true;
+        }else {
+        currNum *= num;
+        sequence++;
+
+        }
+    }
+    cout << "The geometric mean is: " << pow(currNum, (1.0 /sequence)) <<endl;
+
     return 0;
 }
