ed3d097e51176a1a6b83331ede6eae0b152214a3
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-01-30 13:08:32 -0500

    solve hw4 q6

--- week_four/hw/wb886_hw4_q6.cpp
+++ week_four/hw/wb886_hw4_q6.cpp
@@ -3,10 +3,45 @@ using namespace std;
 
 int main()
 {
-    int input;
+    int input, remainder, temp, even, odd, divider;
     cout <<"Please enter a positive integer: ";
     cin >> input;
 
+    for(int digit = 1; digit <= input; digit++){
+            temp = digit;
+            even = 0;
+            odd = 0;
+        while (temp > 0){
+
+            remainder = digit % 10;
+            if(remainder % 2 == 0){
+                even++;
+            } else {
+                odd++;
+            }
+
+            divider = digit / 10;
+            if(divider % 2 == 0){
+                even++;
+            } else {
+                odd++;
+            }
+            
+            temp /= 10;
+        }
+
+        if(even > odd){
+            cout << digit << endl;
+        }
+    }
 
     return 0;
 }
+
+
+
+//   if(digit < 10){
+//             if(digit % 2 == 0){
+//                 cout << digit << endl;
+//             }
+//         }
