69ebd3f5729345cb85efa8df0188675bedd9b332
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-01-30 13:37:16 -0500

    solve hw4 q4 part a

--- week_four/hw/wb886_hw4_q4.cpp
+++ week_four/hw/wb886_hw4_q4.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    int sequence;
+    int sequence, num, currNum = 1;
     cout << "Section A"<<endl;
     cout << "Please enter the length of the sequence: ";
     cin >> sequence;
+
+    cout << "Please enter your sequence"<< endl;
+    for(int numInSequence = 1; numInSequence <= sequence; numInSequence++){
+        cin >> num;
+        currNum *= num;
+
+        if(numInSequence == sequence)
+            cout << "The geometric mean is: " << pow(currNum, (1.0 /sequence)) <<endl;
+    }
 
 
     cout << "Section B"<<endl;
