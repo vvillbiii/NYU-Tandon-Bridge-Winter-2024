d7e0ccc5b9b41777e9fb2ef5b93f9f35bbde9966
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-02-01 17:18:00 -0500

    update hw4 q1

--- week_four/hw/wb886_hw4_q1.cpp
+++ week_four/hw/wb886_hw4_q1.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main()
 {
     int inputNum, i;
-    cout << "Please enter a positive integer: ";
-    cin >> inputNum;
 
     cout << "Section A"<<endl;
+    cout << "Please enter a positive integer: ";
+    cin >> inputNum;
     while(i <= inputNum * 2){
         if(i % 2 == 0){
             cout << i <<endl;
@@ -15,8 +15,11 @@ int main()
         i++;
     }
 
-
+    inputNum = 0;
     cout << "Section B"<<endl;
+
+    cout << "Please enter a positive integer: ";
+    cin >> inputNum;
     for(i = 1; i <= inputNum * 2; i++){
         if(i % 2 == 0){
             cout << i << endl;
