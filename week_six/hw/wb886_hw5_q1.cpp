5e37970c752dd61868572c7f76cc8aa74982ff09
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-02-07 09:53:58 -0500

    start hw5 q1

---
+++ week_six/hw/wb886_hw5_q1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int userInput;
+    cout <<"Please enter a positive integer: " <<endl;
+    cin >> userInput;
+
+    
+    return 0;
+}
