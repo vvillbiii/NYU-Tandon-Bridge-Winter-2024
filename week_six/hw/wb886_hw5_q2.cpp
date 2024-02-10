a0c0d2fd4f66cab13de36311ed0fed833ea4e23b
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-02-09 20:45:30 -0500

    reconstruct hw5 q2 while loop

--- week_six/hw/wb886_hw5_q2.cpp
+++ week_six/hw/wb886_hw5_q2.cpp
@@ -17,27 +17,29 @@ int main()
 
     cout<< "I thought of a number between 1 and 100! Try to guess it."<<endl;
     while(attempts > 1 || userGuess != x1){
-          if(userGuess == x1){
-                cout<<" Congrats! You guessed my number in "<< attempts	<< " guesses.";
-                break;
-            }
 
-            cout <<"Range: [" << minRange << ", " << maxRange << "], Number of guesses left: " << attempts <<endl;
-            cout << "Your guess: ";
-            cin >> userGuess;
-            
-            if(userGuess < x1){
-                cout<< "Wrong! My number is bigger." <<endl;
-                minRange = userGuess + 1;
-            } else {
-                cout<< "Wrong! My number is smaller."<<endl;
-                maxRange = userGuess - 1;
-            }
-          
+        cout <<"Range: [" << minRange << ", " << maxRange << "], Number of guesses left: " << attempts <<endl;
+        cout << "Your guess: ";
+        cin >> userGuess;
+
+        if(userGuess == x1){
+            cout<<"Congrats! You guessed my number in "<< attempts	<< " guesses.";
+            break;
+        }
+
         if(attempts == 1 && userGuess != x1){
             cout <<"Out of guesses! My number is "<< x1<< endl;
             break;
         }
+            
+        if(userGuess < x1){
+            cout<< "Wrong! My number is bigger." <<endl;
+            minRange = userGuess + 1;
+        } else {
+            cout<< "Wrong! My number is smaller."<<endl;
+            maxRange = userGuess - 1;
+        }
+          
 
 
         attempts--;
