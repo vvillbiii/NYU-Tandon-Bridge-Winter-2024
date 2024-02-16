23f47f86eda1816a3b23d9e6960a5ffc80210df6
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-02-16 18:20:03 -0500

    add printShiftedTriangle function

--- week_seven_eight/hw/wb886_hw6_q2.cpp
+++ week_seven_eight/hw/wb886_hw6_q2.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void printShiftedTriangle(int n, int m, char symbol);
+
+
 int main()
 {
+    printShiftedTriangle(3, 4, '+');
+    
     return 0;
+}
+
+
+void printShiftedTriangle(int n, int m, char symbol){
+    for(int lines = 1; lines <= n; lines++){
+        cout << lines << endl;
+    }
+
 }
