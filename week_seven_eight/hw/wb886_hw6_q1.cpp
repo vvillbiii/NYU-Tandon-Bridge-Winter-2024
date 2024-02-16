44a3236cf6d2ef676cd85c8462bc4f01ed4cba01
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-02-16 17:56:38 -0500

    sudo code fib sequence

--- week_seven_eight/hw/wb886_hw6_q1.cpp
+++ week_seven_eight/hw/wb886_hw6_q1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // The Fibonacci numbers sequence, Fn, is defined as follows:
-// F1 is1,F2 is1,and Fn =Fn-1 +Fn-2 forn=3,4,5,...
+// F1 is 1,F2 is 1,and Fn =Fn-1 +Fn-2 form=3,4,5,...
 // In other words, each number is the sum of the previous two numbers. The first 10 numbers in Fibonacci sequence are: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
 
 int fib(int n);
@@ -21,6 +21,37 @@ int main()
 }
 
 int fib(int num){
+    // fib = f1 + f2 
+    //  Each number is the sum of the two before it.
+    /*
+        f1 = 1 and f2 = 0 fib is 1
+        1 + 0 = 1
 
-    return 0;
+        make f1 = fib(1) and f2 = old f1(1)
+        1 + 1 = 2
+        
+        make f1 = fib(2) and f2 = old f1(1)
+        2 + 1 = 3
+
+        make f1 = fib(3) and f2 = old f1(2)
+        3 + 2 = 5
+
+        make f1 = fib(5) and f2 = old f1(3)
+        5 + 3 = 8
+
+        make f1 = fib(8) and f2 = old f1(5)
+        8 + 5 = 13
+        
+    */
+
+    int f1, f2, fib;
+
+    f1 = 1;
+    f2 = 0;
+
+    for(int i = 1; i <= num; i++){
+     
+    }
+
+    return fib;
 }
