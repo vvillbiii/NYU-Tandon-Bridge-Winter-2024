d57291d0b934fe02ce33c0b2192231549868af0a
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-02-16 17:25:06 -0500

    init fib fun for hw6 q1

--- week_seven_eight/hw/wb886_hw6_q1.cpp
+++ week_seven_eight/hw/wb886_hw6_q1.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 
+// The Fibonacci numbers sequence, Fn, is defined as follows:
+// F1 is1,F2 is1,and Fn =Fn-1 +Fn-2 forn=3,4,5,...
+// In other words, each number is the sum of the previous two numbers. The first 10 numbers in Fibonacci sequence are: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+
+int fib(int n);
+
 int main()
 {
 
+    return 0;
+}
+
+int fib(int num){
+    
     return 0;
 }
