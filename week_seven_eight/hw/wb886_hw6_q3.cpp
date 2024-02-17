66e52f27278cdce32679879caf594ea229279dde
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-02-17 18:09:38 -0500

    add comment to hw6 q3

--- week_seven_eight/hw/wb886_hw6_q3.cpp
+++ week_seven_eight/hw/wb886_hw6_q3.cpp
@@ -6,7 +6,7 @@ double eApprox(int n);
 int main()
 {
   
-
+    // This for testing the function 
     cout.precision(30);
     for (int n = 1; n <= 15; n++) {
         cout<<"n = "<<n<<'\t'<<eApprox(n)<<endl; 
