19d2e6a2c669d6fb9fc6d486d024e7acac81227c
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-02-16 19:04:10 -0500

    update fib comment

--- week_seven_eight/hw/wb886_hw6_q1.cpp
+++ week_seven_eight/hw/wb886_hw6_q1.cpp
@@ -26,21 +26,22 @@ int fib(int num){
         f1 = 1 and f2 = 0 fib is 1
         1 + 0 = 1
 
-        make f1 = fib(1) and f2 = old f1(1)
+        make f1 = fib(1) and f2 = old f1(1) fib = 2
         1 + 1 = 2
         
-        make f1 = fib(2) and f2 = old f1(1)
+        make f1 = fib(2) and f2 = old f1(1) fib = 3
         2 + 1 = 3
 
-        make f1 = fib(3) and f2 = old f1(2)
+        make f1 = fib(3) and f2 = old f1(2) fib = 5
         3 + 2 = 5
 
-        make f1 = fib(5) and f2 = old f1(3)
+        make f1 = fib(5) and f2 = old f1(3) fib = 8
         5 + 3 = 8
 
-        make f1 = fib(8) and f2 = old f1(5)
+        make f1 = fib(8) and f2 = old f1(5) fib = 13 
         8 + 5 = 13
         
+        so on and so on
     */
 
     int f1, f2, prevf1, fibNum;
