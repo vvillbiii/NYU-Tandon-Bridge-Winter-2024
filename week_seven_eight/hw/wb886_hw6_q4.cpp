19626661467e172c1fd562c30604c91e4f33ba2b
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-02-18 22:45:05 -0500

    loop through i on print divisors

--- week_seven_eight/hw/wb886_hw6_q4.cpp
+++ week_seven_eight/hw/wb886_hw6_q4.cpp
@@ -5,9 +5,14 @@ void printDivisors(int num);
 
 int main()
 {
-  
+    printDivisors(100);
 
     return 0;
 }
 
 
+void printDivisors(int num){
+    for(int i = 0; i < num; i++){
+        cout<< i;
+    }
+}
