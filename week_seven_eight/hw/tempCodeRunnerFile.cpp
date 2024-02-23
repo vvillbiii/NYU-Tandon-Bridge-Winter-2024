0bbc16650b78a1d15a3f2db62424446435732c55
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-02-23 18:33:37 -0500

    delete temp code run file

--- week_seven_eight/hw/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
- // for(int space = 1 ; space <= 2 * m - lines - 1; space++){
-        //     cout<< " ";
-        // }
-       
-
-        // for(int sign = 1; sign <= 2 * lines - 1; sign++){   
-        //     cout<<symbol;
-        // }
