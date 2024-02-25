fd34c879d3ca4c8556f4acfb851e06b2b7d0f1e0
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-02-25 17:50:04 -0500

    print out days of the week

--- week_seven_eight/hw_7/wb886_hw7_q1.cpp
+++ week_seven_eight/hw_7/wb886_hw7_q1.cpp
@@ -6,8 +6,34 @@ void printYearCalender(int year, int startingDay);
 
 int main()
 {
+    printMonthCalender(31, 4);
+
+    return 0;
+}
 
 
+int printMonthCalender(int numOfDays, int startingDay){
+    for(int i = 1; i <= 7; i++){
+        if(i ==1 ){
+            cout<< " Mon"<< "\t";
+        } else if(i ==2){
+            cout<< "Tue"<< "\t";
+        } else if(i ==3){
+            cout<< "Wed"<< "\t";
+        } else if(i ==4){
+            cout<< "Thr"<< "\t";
+        } else if(i ==5){
+            cout<< " Fri"<< "\t";
+        } else if(i ==6){
+            cout<< " Sat"<< "\t";
+        } else {
+            cout <<" Sun" <<"\t";
+        }
+        
+       
+    }
+    
+    
     return 0;
 }
 
