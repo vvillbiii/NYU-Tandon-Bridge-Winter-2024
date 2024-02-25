c0b652709d55dd5af0f9392de38877ed5349e32e
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-02-25 17:51:31 -0500

    fix formatting

--- week_seven_eight/hw_7/wb886_hw7_q1.cpp
+++ week_seven_eight/hw_7/wb886_hw7_q1.cpp
@@ -14,20 +14,20 @@ int main()
 
 int printMonthCalender(int numOfDays, int startingDay){
     for(int i = 1; i <= 7; i++){
-        if(i ==1 ){
-            cout<< " Mon"<< "\t";
-        } else if(i ==2){
-            cout<< "Tue"<< "\t";
-        } else if(i ==3){
-            cout<< "Wed"<< "\t";
-        } else if(i ==4){
-            cout<< "Thr"<< "\t";
-        } else if(i ==5){
-            cout<< " Fri"<< "\t";
-        } else if(i ==6){
-            cout<< " Sat"<< "\t";
+        if(i == 1){
+            cout<<"Mon"<<"\t";
+        } else if(i == 2){
+            cout<<"Tue"<<"\t";
+        } else if(i == 3){
+            cout<< "Wed"<<"\t";
+        } else if(i == 4){
+            cout<< "Thr"<<"\t";
+        } else if(i == 5){
+            cout<<"Fri"<<"\t";
+        } else if(i == 6){
+            cout<<"Sat"<<"\t";
         } else {
-            cout <<" Sun" <<"\t";
+            cout <<"Sun"<<"\t";
         }
         
        
