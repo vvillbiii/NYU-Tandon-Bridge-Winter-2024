f3303862debbc11ea8360a7ae5ffd48c5b0a1798
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-02-28 22:25:00 -0500

    print year with starting days

--- week_seven_eight/hw_7/wb886_hw7_q1.cpp
+++ week_seven_eight/hw_7/wb886_hw7_q1.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int printMonthCalender(int numOfDays, int startingDay);
 void printYearCalender(int year, int startingDay);
+int DaysInFeb(int year);
 
 int main()
 {
-    printMonthCalender(31, 4);
+    // printMonthCalender(31, 4);
     printYearCalender(2016, 5);
 
     return 0;
@@ -34,50 +35,59 @@ int printMonthCalender(int numOfDays, int startingDay){
     return lastDay;
 }
 
+int DaysInFeb(int year) {
+ bool leapYear = (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
+  if (leapYear) {
+    return 29;
+  } else {
+    return 28;
+  }
+}
+
 
 void printYearCalender(int year, int startingDay){
     cout<<"January "<< year<<endl;
-    printMonthCalender(31, startingDay);
+    startingDay = printMonthCalender(31, startingDay) + 1;
     cout<<endl;
 
     cout<<"February "<< year<<endl;
-    printMonthCalender(31, startingDay);
+    startingDay = printMonthCalender(DaysInFeb(year), startingDay) + 1;
     cout<<endl;
-    
+
     cout<<"March "<< year<<endl;
-    printMonthCalender(31, startingDay);
+    startingDay = printMonthCalender(31, startingDay) + 1;
     cout<<endl;
 
     cout<<"April "<< year<<endl;
-    printMonthCalender(31, startingDay);
+    startingDay = printMonthCalender(30, startingDay) + 1;
     cout<<endl;
 
     cout<<"May "<< year<<endl;
-    printMonthCalender(31, startingDay);
+    startingDay = printMonthCalender(31, startingDay) + 1;
     cout<<endl;
 
     cout<<"June "<< year<<endl;
-    printMonthCalender(31, startingDay);
+    startingDay = printMonthCalender(30, startingDay) + 1;
     cout<<endl;
 
     cout<<"July "<< year<<endl;
-    printMonthCalender(31, startingDay);
+    startingDay = printMonthCalender(31, startingDay) + 1;
     cout<<endl;
 
     cout<<"August "<< year<<endl;
-    printMonthCalender(31, startingDay);
+    startingDay = printMonthCalender(31, startingDay) + 1;
     cout<<endl;
 
     cout<<"September "<< year<<endl;
-    printMonthCalender(31, startingDay);
+    startingDay = printMonthCalender(30, startingDay) + 1;
     cout<<endl;
 
     cout<<"October "<< year<<endl;
-    printMonthCalender(31, startingDay);
+    startingDay = printMonthCalender(31, startingDay) + 1;
     cout<<endl;
 
     cout<<"November "<< year<<endl;
-    printMonthCalender(31, startingDay);
+    startingDay = printMonthCalender(30, startingDay) + 1;
     cout<<endl;
 
     cout<<"December "<< year<<endl;
