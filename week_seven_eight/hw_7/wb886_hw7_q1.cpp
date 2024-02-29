a6161c7cc3436a285994849f0d15d5cfd548915f
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-02-28 22:16:36 -0500

    print calender

--- week_seven_eight/hw_7/wb886_hw7_q1.cpp
+++ week_seven_eight/hw_7/wb886_hw7_q1.cpp
@@ -7,6 +7,7 @@ void printYearCalender(int year, int startingDay);
 int main()
 {
     printMonthCalender(31, 4);
+    printYearCalender(2016, 5);
 
     return 0;
 }
@@ -35,5 +36,52 @@ int printMonthCalender(int numOfDays, int startingDay){
 
 
 void printYearCalender(int year, int startingDay){
+    cout<<"January "<< year<<endl;
+    printMonthCalender(31, startingDay);
+    cout<<endl;
+
+    cout<<"February "<< year<<endl;
+    printMonthCalender(31, startingDay);
+    cout<<endl;
     
+    cout<<"March "<< year<<endl;
+    printMonthCalender(31, startingDay);
+    cout<<endl;
+
+    cout<<"April "<< year<<endl;
+    printMonthCalender(31, startingDay);
+    cout<<endl;
+
+    cout<<"May "<< year<<endl;
+    printMonthCalender(31, startingDay);
+    cout<<endl;
+
+    cout<<"June "<< year<<endl;
+    printMonthCalender(31, startingDay);
+    cout<<endl;
+
+    cout<<"July "<< year<<endl;
+    printMonthCalender(31, startingDay);
+    cout<<endl;
+
+    cout<<"August "<< year<<endl;
+    printMonthCalender(31, startingDay);
+    cout<<endl;
+
+    cout<<"September "<< year<<endl;
+    printMonthCalender(31, startingDay);
+    cout<<endl;
+
+    cout<<"October "<< year<<endl;
+    printMonthCalender(31, startingDay);
+    cout<<endl;
+
+    cout<<"November "<< year<<endl;
+    printMonthCalender(31, startingDay);
+    cout<<endl;
+
+    cout<<"December "<< year<<endl;
+    printMonthCalender(31, startingDay);
+    cout<<endl;
+              
 }
