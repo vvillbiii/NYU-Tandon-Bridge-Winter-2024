1a4439574f6bc7a434f7378eb98d205a7fe2e783
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-04 23:45:00 -0500

    init minInArray function

--- week_nine/hw/wb886_hw8_q1.cpp
+++ week_nine/hw/wb886_hw8_q1.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int minInArray(int arr[], int arrSize);
+
 int main()
 {
+    cout<<"Please enter 20 integers separated by a space:"<<endl;
  
 
     return 0;
 }
 
+int minInArray(int arr[], int arrSize){
 
+}
