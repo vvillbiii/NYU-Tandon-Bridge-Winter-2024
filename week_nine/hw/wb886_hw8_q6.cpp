59760cc3dad8a5d4d6d449cb8ff35709082e5f5f
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-05 18:37:48 -0500

    init hw8 q6

---
+++ week_nine/hw/wb886_hw8_q6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string line;
+
+    cout << "Please enter a line of text:"<<endl;
+    getline(cin, line);
+
+   cout<<line;
+
+    return 0;
+}
+
+
+
