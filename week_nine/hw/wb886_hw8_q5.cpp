ca5f03d1acd345a336aa09d3e44ac44027df9412
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-05 18:35:10 -0500

    delete unused files

--- week_nine/hw/wb886_hw8_q5.cpp
+++ week_nine/hw/wb886_hw8_q5.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main() {
     string firstName, middleName, lastName;
 
-    cout << "Please enter your name (first, middle, and last): ";
-    cin >> firstName >> middleName >> lastName;
-    cout << lastName << ", " << firstName << " " << middleName[0] << ".\n";
+    cout<<"Please enter your name (first, middle, and last): ";
+    cin>>firstName>>middleName>>lastName;
+    cout << lastName << ", " << firstName << " " << middleName[0] << ".";
 
     return 0;
 }
