e256c0fe0bb8a835c5fd89480f61f92d344f3a63
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-05 13:07:38 -0500

    solve hw8 q2

--- week_nine/hw/wb886_hw8_q2.cpp
+++ week_nine/hw/wb886_hw8_q2.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 
-int main()
-{
+bool isPalindrome(string str);
 
+int main() {
+    string word;
+
+    cout<<"Please enter a word: ";
+    cin>>word;
+
+    if (isPalindrome(word)) {
+        cout << word << " is a palindrome\n";
+    } else {
+        cout << word << " is not a palindrome\n";
+    }
 
     return 0;
 }
 
+bool isPalindrome(string str) {
+    int strLength = str.length();
+
+    for (int i = 0; i < strLength / 2; i++) {
+        if (str[i] != str[strLength - i - 1]) {
+            return false;
+        }
+    }
+
+    return true;
+}
