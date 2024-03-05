7fc30c8a04c59613160ec7538ce2641b04a9e033
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-05 09:28:40 -0500

    start with myArray

--- week_nine/hw/wb886_hw8_q1.cpp
+++ week_nine/hw/wb886_hw8_q1.cpp
@@ -3,14 +3,20 @@ using namespace std;
 
 int minInArray(int arr[], int arrSize);
 
+const int SIZE = 20;
+
 int main()
 {
+    int myArray[SIZE];
     cout<<"Please enter 20 integers separated by a space:"<<endl;
- 
+    for(int i = 0; i <= SIZE; i++){
+        cin>>myArray[i];
+    }
 
     return 0;
 }
 
 int minInArray(int arr[], int arrSize){
 
+    return 1;   
 }
