86fa7af73bf13e8e7824ef54d456d2a2e9232bdd
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-08 19:49:33 -0500

    start hw8 q3

--- week_nine/hw/wb886_hw8_q3.cpp
+++ week_nine/hw/wb886_hw8_q3.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
 using namespace std;
 
+void printArray(int arr[], int arrSize);
+void reverseArray(int arr[], int arrSize);
+void removeOdd(int arr[], int& arrSize);
+void splitParity(int arr[], int arrSize);
+bool isEven(int n);
+
 int main() {
+
+    int arr1[10] = {9, 2, 14, 12, -3};
+    int arr1Size = 5;
+
+    int arr2[10] = {21, 12, 6, 7, 14};
+    int arr2Size = 5;
+
+    int arr3[10] = {3, 6, 4, 1, 12};
+    int arr3Size = 5;
+
+    reverseArray(arr1, arr1Size); 
+    printArray(arr1, arr1Size);
+
+    removeOdd(arr2, arr2Size); 
+    printArray(arr2, arr2Size);
+
+    splitParity(arr3, arr3Size); 
+    printArray(arr3, arr3Size);
   
     return 0;
 }
 
-
+void printArray(int arr[], int arrSize){ int i;
+for (i = 0; i < arrSize; i++) { cout<<arr[i]<<' ';
+}
+cout<<endl;
+}
 
