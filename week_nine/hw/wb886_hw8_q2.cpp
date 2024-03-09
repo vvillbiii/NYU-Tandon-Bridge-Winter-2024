8a99174bcd8a68459d740c40bb5eeb27f63b2dec
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-08 19:46:22 -0500

    edit palindrome if statement

--- week_nine/hw/wb886_hw8_q2.cpp
+++ week_nine/hw/wb886_hw8_q2.cpp
@@ -12,9 +12,9 @@ int main() {
     cin>>word;
 
     if (isPalindrome(word)) {
-        cout << word << " is a palindrome\n";
+        cout << word << " is a palindrome"<<endl;
     } else {
-        cout << word << " is not a palindrome\n";
+        cout << word << " is not a palindrome"<<endl;
     }
 
     return 0;
