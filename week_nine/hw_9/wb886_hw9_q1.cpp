082c1cca438da6cc51a0d1e305edeb758dbfa770
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-11 23:59:09 -0400

    solve hw 9 q1

--- week_nine/hw_9/wb886_hw9_q1.cpp
+++ week_nine/hw_9/wb886_hw9_q1.cpp
@@ -3,25 +3,35 @@
 using namespace std;
 
 int getWordCount(string sentence);
-bool isNotLetter(char letter);
+bool isLetter(char letter);
+void getLetter(string sentence, int WordCount, int letterCount []);
 
 int main() {
 
     string sentence;
     int wordCount;
+    int letterCount[26] = {0};
     cout<<"Please enter a line of text:"<<endl;
     getline(cin, sentence);
 
     wordCount = getWordCount(sentence);
+    getLetter(sentence, wordCount, letterCount);
+
     cout<<wordCount<< " words" <<endl;
+
+    for (int i = 0; i < 26; i++) {
+        if (letterCount[i] > 0) {
+            cout << letterCount[i] << " " << (char)(i + 'a') << endl;
+        }
+    }
 
     return 0;
 }
 
 int getWordCount(string sentence){
-    int wordCount =0;
+    int wordCount = 0;
     for(int i = 0; i < sentence.length(); i++){
-        if(isNotLetter(sentence[i])){
+        if(!isLetter(sentence[i])){
             wordCount++;
         } 
     }
@@ -29,9 +39,14 @@ int getWordCount(string sentence){
     return wordCount;
 }
 
-bool isNotLetter(char letter){
-    if(letter == ' ' || letter == '.' || letter == ',' ){
-        return true;
+void getLetter(string sentence, int count, int letterCount []){   
+    for(int i = 0; i <= sentence.length(); i++){
+        if(isLetter(sentence[i])){
+           letterCount[tolower(sentence[i]) - 'a']++;
+        }
     }
-    return false;
+}
+
+bool isLetter(char letter) {
+    return (letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z');
 }
