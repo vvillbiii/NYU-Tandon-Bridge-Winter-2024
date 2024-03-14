44bd8d891cf29b1023b73b46adc36c03ca686b86
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-13 22:17:21 -0400

    start hw9 q2

--- week_nine/hw_9/wb886_hw9_q2.cpp
+++ week_nine/hw_9/wb886_hw9_q2.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+    string firstSentence;
+    string secondSentence;
+
+    cout << "Are these strings anagrams?" << endl;
+    cout << "Enter first string: ";
+    getline(cin, firstSentence);
+    cout << "Enter second string: ";
+    getline(cin, secondSentence);
 
     return 0;
 }
