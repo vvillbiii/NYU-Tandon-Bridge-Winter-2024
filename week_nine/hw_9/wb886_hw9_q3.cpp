09395076195b51beffc9508200ff801fad549b60
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-13 22:59:44 -0400

    add functions for q3 hw9

--- week_nine/hw_9/wb886_hw9_q3.cpp
+++ week_nine/hw_9/wb886_hw9_q3.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+int* getPosNums1(int* arr, int arrSize, int& outPosArrSize);
+int* getPosNums2(int* arr, int arrSize, int* outPosArrSizePtr);
+void getPosNums3(int* arr, int arrSize, int*& outPosArr, int& outPosArrSize);
+void getPosNums4(int* arr, int arrSize, int** outPosArrPtr, int* outPosArrSizePtr);
+
 int main() {
    
 
