ed5c1bb0d3deee61c734634a3c72e178fd08b9d4
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-14 21:09:05 -0400

    print array

--- week_nine/hw_9/wb886_hw9_q4.cpp
+++ week_nine/hw_9/wb886_hw9_q4.cpp
@@ -2,11 +2,23 @@
 using namespace std;
 
 void oddsKeepEvensFlip(int arr[], int arrSize);
+void printArray(int arr[], int arrSize);
+
+const int ARRSIZE = 6;
 
 int main() {
+    int arr[ARRSIZE] = {5, 2, 11, 7, 6, 4};
 
+    printArray(arr, ARRSIZE);
 
    
 
     return 0;
+}
+
+void printArray(int arr[], int arrSize){
+    for(int i = 0; i < arrSize; i++){
+        cout<<arr[i]<< " ";
+    }
+    cout<<endl;
 }
