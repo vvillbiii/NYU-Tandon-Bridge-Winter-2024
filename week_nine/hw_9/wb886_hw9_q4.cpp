033bcd88b8bf74668db7e4257b0ac82975098b2a
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-14 21:15:58 -0400

    print evens in odds keep even flip

--- week_nine/hw_9/wb886_hw9_q4.cpp
+++ week_nine/hw_9/wb886_hw9_q4.cpp
@@ -9,11 +9,23 @@ const int ARRSIZE = 6;
 int main() {
     int arr[ARRSIZE] = {5, 2, 11, 7, 6, 4};
 
+    oddsKeepEvensFlip(arr, ARRSIZE);
     printArray(arr, ARRSIZE);
 
    
 
     return 0;
+}
+
+void oddsKeepEvensFlip(int arr[], int arrSize){
+    int start = 0;
+     for(int i = 0; i < arrSize; i++){
+        if(arr[i] % 2 == 0){
+            cout<<arr[i]<< " ";
+        }
+    }
+
+    cout<<"print evens"<<endl;
 }
 
 void printArray(int arr[], int arrSize){
