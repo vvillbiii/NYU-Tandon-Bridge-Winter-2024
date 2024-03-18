1b5883c4d8817cb324c4c97a76880f661383f288
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-17 21:32:58 -0400

    add is letter func

--- week_ten/hw_10/wb886_hw10_q1.cpp
+++ week_ten/hw_10/wb886_hw10_q1.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 
 string* createWordsArray(string sentence, int& outWordsArrSize);
+bool isLetter(char letter);
 
 int main()
 {
  
     return 0;
+}
+
+bool isLetter(char letter) {
+    return (letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z');
 }
