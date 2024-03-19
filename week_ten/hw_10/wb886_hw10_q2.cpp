009cbad3c686e56f256a712928a9a6245125a24b
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-18 22:39:56 -0400

    init arr

--- week_ten/hw_10/wb886_hw10_q2.cpp
+++ week_ten/hw_10/wb886_hw10_q2.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
 
+const int ARRSIZE = 6;
+
 int* findMissing(int arr[], int n, int& res_arr_size);
 
 int main()
 {
+     int arr[ARRSIZE] = {3, 1, 3, 0, 6, 4};
  
     return 0;
 }
