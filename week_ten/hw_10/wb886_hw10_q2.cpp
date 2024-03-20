27f63410af5e4e24f8aaacec1bdddd5eaa9e9b26
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-19 20:34:54 -0400

    add findmissing func

--- week_ten/hw_10/wb886_hw10_q2.cpp
+++ week_ten/hw_10/wb886_hw10_q2.cpp
@@ -8,6 +8,14 @@ int* findMissing(int arr[], int n, int& res_arr_size);
 int main()
 {
      int arr[ARRSIZE] = {3, 1, 3, 0, 6, 4};
+     int * arrPtr = NULL;
+     int reSizeArr = 0;
+
+     findMissing(arr,ARRSIZE,reSizeArr);
  
     return 0;
+}
+
+int* findMissing(int arr[], int &tgt, int &resArrSize){
+
 }
