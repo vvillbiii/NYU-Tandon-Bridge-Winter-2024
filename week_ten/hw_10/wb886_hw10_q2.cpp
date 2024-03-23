d364b67a2603082901914b79c4d9bd46ff005a03
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-22 23:41:53 -0400

    refactor hw10 q2

--- week_ten/hw_10/wb886_hw10_q2.cpp
+++ week_ten/hw_10/wb886_hw10_q2.cpp
@@ -4,52 +4,62 @@ using namespace std;
 const int ARRSIZE = 6;
 
 int* findMissing(int arr[], int n, int& resArrSize);
-void  printArray(int arr[], int arrSize);
+void printArray(int arr[], int arrSize);
 
 int main()
 {
-     int arr[ARRSIZE] = {3, 1, 3, 0, 6, 4};
-     int reSizeArr = 0;
+    int arr[ARRSIZE] = {3, 1, 3, 0, 6, 4};
+    int reSizeArr = 0;
 
     int* arrPtr = findMissing(arr,ARRSIZE,reSizeArr);
     cout << "Original: ";
     printArray(arr, ARRSIZE);
     cout << endl;
-    cout << "Modified: ";
+    cout << "Missing: ";
     printArray(arrPtr, reSizeArr);
     cout << endl;
+
+    delete[] arrPtr;
  
     return 0;
 }
 
 int* findMissing(int arr[], int n, int &resArrSize){
-   int countValues[n + 1];
-    resArrSize = n+1;
+    int* countValues = new int[n + 1]();
     for (int i = 0; i < n; i++) {
-        if (countValues[arr[i]] == 0) resArrSize--;
-        countValues[arr[i]]++;
-    }
-    
-    int *outputArray = new int[resArrSize];
-    for (int i = 0, j = 0; i < n + 1; i++) {
-        if (countValues[i] == 0) {
-            outputArray[j++] = i;
+        if (arr[i] >= 0 && arr[i] <= n) {
+            countValues[arr[i]]++;
         }
     }
+
+    int count = 0;
+    for (int i = 0; i <= n; i++) {
+        if (countValues[i] == 0) {
+            count++;
+        }
+    }
+
+   int* outputArray = new int[count];
+    int index = 0;
+    for (int i = 0; i <= n; i++) {
+        if (countValues[i] == 0) {
+            outputArray[index++] = i;
+        }
+    }
+
+    resArrSize = count;
+    delete[] countValues;
     return outputArray;  
 
 }
 
 void  printArray(int arr[], int arrSize) {
     cout << "[";
-
     for (int i = 0; i < arrSize; i++) {
-        cout << "\"" << arr[i] << "\"";
-
+        cout << arr[i];
         if (i != arrSize - 1) {
             cout << ", ";
         }
     }
-
     cout << "]"<<endl;
 }
