c89d4fd8b59197e5b4689eb4cc652073cf6a289f
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-29 19:57:24 -0400

    remove unused functions

--- hw11/wb886_hw11_q4.cpp
+++ hw11/wb886_hw11_q4.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int cheapestCost(int arr[], int arrSize);
-int getMin(int input1, int input2);
-void printArray(int arr[], int arrSize);
 
 int main()
 {
