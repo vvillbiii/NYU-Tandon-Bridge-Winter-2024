aee1b2dcdd7670cb9906ea1c13701e50b6741c51
Will Bowles <94205501+vvillbiii@users.noreply.github.com>
2024-03-29 21:26:11 -0400

    remove unused run file

--- hw11/wb886_hw11_q4.cpp
+++ hw11/wb886_hw11_q4.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int minCost(int costs[], int n);
 
 int main()
 {
+    int costs[] = {0, 3, 80, 6, 57, 10};
+    int size = 6;
+
+    cout << "Minimum cost: " << minCost(costs, size) << endl;
     return 0;
 }
